1172f8fd8eb14fe16ae904c8a8b5db47766469d7
Мирошниченко Aндрей <miroshnychenkoandrew@gmail.com>
2014-09-23 00:45:21 +0300

    Lecture 10

---
+++ app/src/main/java/com/collosteam/sitereader/data/ResponseItem.java
@@ -0,0 +1,10 @@
+package com.collosteam.sitereader.data;
+
+/**
+ * Created by Collos on 9/22/2014.
+ */
+public class ResponseItem {
+
+    public int response
+
+}
---
+++ app/src/main/java/com/collosteam/sitereader/net/NetworkManager.java
@@ -0,0 +1,7 @@
+package com.collosteam.sitereader.net;
+
+/**
+ * Created by Collos on 9/22/2014.
+ */
+public class NetworkManager {
+}
